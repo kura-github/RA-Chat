06a1b8aa10a0b5264eeebf1d4be0017973b04686
kurakawa <52532191+kura-github@users.noreply.github.com>
2020-07-30 22:27:41 +0900

    1st commit

---
+++ public/client.js
@@ -0,0 +1,97 @@
+// クライアントからサーバーへの接続要求
+const socket = io.connect();
+
+// 接続時の処理
+// ・サーバーとクライアントの接続が確立すると、
+// 　サーバー側で、'connection'イベント
+// 　クライアント側で、'connect'イベントが発生する
+socket.on('connect', () => {
+    console.log( 'connect' );
+});
+
+
+// 「Join」ボタンを押したときの処理
+$( '#join-form' ).submit(() => {
+        console.log( '#input_nickname :', $( '#input_nickname' ).val() );
+
+        if($('#input_nickname').val())
+        {
+            // サーバーに、イベント名'join' で入力テキストを送信
+
+            //$("#pgss").css({'width':'100%'});
+            
+            socket.emit( 'join', $( '#input_nickname' ).val() , $('#input_room').val());
+
+            $( '#nickname' ).html( $( '#input_nickname' ).val() );
+            $('#room').html($('#input_room').val());
+            $( '#join-screen' ).hide();
+            $( '#chat-screen' ).show();
+        }
+
+        return false;   // フォーム送信はしない
+});
+
+// 「Send」ボタンを押したときの処理
+$( 'form' ).submit(() => {
+        console.log( '#input_message :', $( '#input_message' ).val() );
+
+        if( $('#input_message').val()) {
+            // サーバーに、イベント名'new message' で入力テキストを送信
+            socket.emit('new message', $( '#input_message').val());
+
+            $('#input_message').val('');    // テキストボックスを空にする
+        }
+        return false;   // フォーム送信はしない
+});
+
+$('#leave_button').click(() => {
+    console.log('leaved');
+    socket.emit('disconnect');
+    location.href = 'index.html';
+});
+
+
+$('#input_message').on('input', () => {
+    socket.emit('typing');
+});
+
+// サーバーからのメッセージ拡散に対する処理
+// ・サーバー側のメッセージ拡散時の「io.emit( 'spread message', strMessage );」に対する処理
+socket.on('spread message', ( objMessage ) => {
+        //console.log( 'spread message :', strMessage );
+        console.log( 'spread message :', objMessage );
+
+        // メッセージの整形
+        //const strText = objMessage.strDate + ' - ' + objMessage.strMessage;
+        const strMessage = objMessage.strDate + ' - [' + objMessage.strNickname + '] ' + objMessage.strMessage;
+
+        // 拡散されたメッセージをメッセージリストに追加
+        //const li_element = $( '<li>' ).text( strMessage );
+
+        var element;
+        var box;
+
+        if(objMessage.type === 'system') {
+            element = $('<p class="system_message"></p>').text( strMessage );
+        }
+        else if(objMessage.type === 'send') {
+            element = $('<div class="send_message"></div>').text( strMessage );
+        }
+        else {
+            element = $('<div class="receive_message"></div>').text(strMessage);
+        }
+        
+        box = $('<div class="box"></div>').html(element);
+        //$( '#message_list' ).prepend( li_element ); // リストの一番上に追加
+        $('#message_list').append(box);    // リストの一番下に追加
+} );
+
+/*
+socket.on('receive message', (objMessage) => {
+    console.log('receive message :', objMessage);
+
+    const message = objMessage.strDate + ' - [' + objMessage.strNickname + '] ' + objMessage.strMessage;
+    let li_element = $('<li>').addClass("user_message").text(message);
+    $('#message_list').append(li_element);
+});
+*/
---
+++ server.js
@@ -0,0 +1,168 @@
+'use strict';
+
+// モジュール
+const express = require( 'express' );
+const http = require( 'http' );
+const socketIO = require( 'socket.io' );
+
+// オブジェクト
+const app = express();
+const server = http.Server( app );
+const io = socketIO( server );
+
+// 定数
+const PORT = process.env.PORT || 3000;
+const SYSTEMNICKNAME = '管理人';
+
+// 関数
+// 数字を２桁の文字列に変換
+const toDoubleDigitString = (num) => {
+    return ( "0" + num ).slice( -2 );   // slice( -2 )で後ろから２文字取り出す。
+};
+
+// 時刻文字列の作成（書式は「YY/DD/MM hh:mm ss」）
+const makeTimeString = (time) => {
+    return toDoubleDigitString(time.getFullYear()) + '/' + toDoubleDigitString(time.getMonth() + 1) + '/' + toDoubleDigitString(time.getDate())
+    + ' ' + toDoubleDigitString( time.getHours() ) + ':' + toDoubleDigitString( time.getMinutes());
+}
+
+// グローバル変数
+let iCountUser = 0; // ユーザー数
+let typing = false; //入力中かどうか
+let socketTmp;
+
+// 接続時の処理
+// ・サーバーとクライアントの接続が確立すると、
+// 　サーバー側で、'connection'イベント
+// 　クライアント側で、'connect'イベントが発生する
+io.on('connection', (socket) => {
+    console.log('connection');
+
+    let strNickname = '';	// コネクションごとで固有のニックネーム。イベントをまたいで使用される。
+    let room = '';
+
+        // 切断時の処理
+        // ・クライアントが切断したら、サーバー側では'disconnect'イベントが発生する
+        socket.on('disconnect', () => {
+            console.log( 'disconnect' );
+
+            if(strNickname) {
+                    // ユーザー数の更新
+                    iCountUser--;
+
+                    // メッセージオブジェクトに現在時刻を追加
+                    const strNow = makeTimeString( new Date() );
+
+                    // システムメッセージの作成
+                    const objMessage = {
+                        strNickname: SYSTEMNICKNAME,
+                        strMessage: strNickname + 'さんが退室しました。' + "現在のユーザー数は" + iCountUser + "人です。",
+                        strDate: strNow,
+                        type: 'system'
+                    };
+                
+
+                    // 送信元含む全員に送信
+                    socket.broadcast.emit( 'spread message', objMessage );
+            }
+        });
+
+        // 入室時の処理
+        // ・クライアント側のメッセージ送信時の「socket.emit( 'join', strNickname );」に対する処理【12】
+        socket.on('join', ( strNickname_ , joinRoom_) => {
+                room = joinRoom_;
+                socket.join(room);
+
+                //現在のsocketIDを格納
+                socketTmp = socket.id;
+
+                console.log( 'joined :', strNickname_);
+                console.log(room);
+
+                // コネクションごとで固有のニックネームに設定
+                strNickname = strNickname_;
+
+                // ユーザー数の更新
+                iCountUser++;
+
+                // メッセージオブジェクトに現在時刻を追加
+                const strNow = makeTimeString( new Date() );
+
+                
+                // システムメッセージの作成
+                const objMessage = {
+                    strNickname: SYSTEMNICKNAME,
+                    strMessage: strNickname + 'さんが入室しました。' + "現在のユーザー数は" + iCountUser + "人です。",
+                    strDate: strNow,
+                    type: 'system'
+                };
+
+                // 送信元含む全員に送信
+                io.to(room).emit( 'spread message', objMessage );
+        });
+
+        // 新しいメッセージ受信時の処理
+        // ・クライアント側のメッセージ送信時の「socket.emit( 'new message', $( '#input_message' ).val() );」に対する処理
+        socket.on('new message', (strMessage) => {
+                typing = false;
+                console.log( 'new message', strMessage );
+
+                // 現在時刻の文字列の作成
+                const strNow = makeTimeString( new Date() );
+
+                //送信側か受信側かを分ける
+                let messageType;
+
+                if(socketTmp === socket.id) {
+                    messageType = 'send';
+                }
+                else {
+                    messageType = 'receive';
+                }
+
+                // メッセージオブジェクトの作成
+
+                const objMessage = {
+                    strNickname: strNickname,
+                    strMessage: strMessage,
+                    strDate: strNow,
+                    type: messageType
+                };
+
+                console.log('socket id:', socket.id);
+                // 送信元含む全員に送信
+                //io.emit( 'spread message', strMessage );
+                io.to(room).emit( 'spread message', objMessage );
+        });
+
+        socket.on('typing', () => {
+
+            if(typing === false) {
+                typing = true;
+                console.log('now typing');
+                const strNow = makeTimeString( new Date() );
+                const statusMessage = '入力中です';
+
+                const objMessage = {
+                    strNickname: strNickname,
+                    strMessage: statusMessage,
+                    strDate: strNow,
+                    type: 'system'
+                };
+
+                socket.broadcast.emit('spread message', objMessage);
+            }
+        });
+
+        socket.on('stop typing', () => {
+
+        });
+});
+
+// 公開フォルダの指定
+app.use( express.static( __dirname + '/public' ) );
+
+// サーバーの起動
+server.listen(PORT,() => {
+    console.log( 'Server on port %d', PORT );
+});
