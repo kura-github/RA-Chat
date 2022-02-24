384e7afe4c9100e6d61cdc7617a1eab9d14f89e1
kura-github <kurakawa.kousuke28@gmail.com>
2022-02-24 19:46:48 +0900

    add:プロジェクト新規作成

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<style>
+
+</style>
+
+<template>
+    <div id="chat-screen">
+            <div id="message-list">
+                <!--  ここにメッセージが動的に追加される  -->
+            </div>
+        
+            <div id="emotion-value">
+                <input type="radio" name="emotion" value="laugh">
+                <input type="radio" name="emotion" value="smile" checked>
+                <input type="radio" name="emotion" value="surprise">
+                <input type="radio" name="emotion" value="angry">
+                <input type="radio" name="emotion" value="cry">
+            </div>
+        
+            <div id="emotion-set">
+                <button id="laugh" value="laugh"></button>
+                <button id="smile" value="smile"></button>
+                <button id="surprise" value="surprise"></button>
+                <button id="angry" value="angry"></button>
+                <button id="cry" value="cry"></button>
+            </div>
+        
+            
+        </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent ({
+    name: 'App',
+    setup() {
+        return {
+            helloText: 'Hello,World!'
+        }
+    }
+})
+</script>
---
+++ src/components/MessageForm.vue
@@ -0,0 +1,20 @@
+<template>
+  <form id="message-form" action="">
+    Nickname : <span id="nickname">{{ nickName }}</span> Room : <span id="room">{{ room }}</span>
+    <input type="text" id="input_message" autocomplete="off" placeholder="Message"/>
+    <button type="submit" class="btn-xs btn-success" id="send_button">送信</button>
+  </form>
+</template>
+
+<script>
+export default {
+    props: {
+        nickName: String,
+        room:   Number
+    }
+}
+</script>
+
+<style>
+
+</style>
